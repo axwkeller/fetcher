bc3af333afdb8ce390006d22d4bbc4699238b86b
Alex Keller <51135033+axwkeller@users.noreply.github.com>
2024-09-16 11:46:38 -0600

    Add playwright CI config (#1)
    * add notes and scratch to gitignore
    
    * add basic CI workflow for playwright
    
    * add validate PR workflow and rename playwright.yml to integration-tests.yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,11 +2,14 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'pnpm run build && pnpm run preview',
 		port: 4173
 	},
 	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+
+	// Opt out of parallel tests on CI.
+	workers: process.env.CI ? 1 : undefined
 };
 
 export default config;
