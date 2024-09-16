90810b025e2d7dc7407668d3ca159058efd76432
Alex Keller <axwkeller@gmail.com>
2024-09-16 09:46:40 -0600

    Configure SST/AWS accounts

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,19 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "fetcher",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
-    };
-  },
-  async run() {
-    new sst.aws.SvelteKit("MyWeb");
-  },
+	app(input) {
+		return {
+			name: 'fetcher',
+			removal: input?.stage === 'production' ? 'retain' : 'remove',
+			home: 'aws',
+			providers: {
+				aws: {
+					profile: input.stage === 'production' ? 'axwfetcher-prod' : 'axwfetcher-dev'
+				}
+			}
+		};
+	},
+	async run() {
+		new sst.aws.SvelteKit('MyWeb');
+	}
 });
