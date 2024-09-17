ee9b8ced6577e049ca950147dcfaa7209f338537
Alex Keller <axwkeller@gmail.com>
2024-09-17 14:46:24 -0600

    implement basic fetch page

--- src/routes/fetch/+page.svelte
+++ src/routes/fetch/+page.svelte
@@ -1,1 +1,60 @@
+<script lang="ts">
+	import type { Dog, DogSearchResult } from '$lib/models/Dog';
+	import Api from '$lib/utils/Api';
+	import { onMount } from 'svelte';
+
+	let loadingSearch = $state(false);
+	let loadingDogs = $state(false);
+	let searchResults: DogSearchResult = $state({ resultIds: [], total: 0, next: '', prev: '' });
+	let dogs: Dog[] = $state([]);
+
+	onMount(async () => {
+		await searchDogs();
+		await getDogs(searchResults.resultIds);
+	});
+
+	async function searchDogs() {
+		loadingSearch = true;
+		try {
+			const response = await Api.searchDogs();
+			if (response.ok) {
+				const body = await response.json();
+				searchResults = body;
+			}
+		} catch (err) {
+			console.error(err);
+		} finally {
+			loadingSearch = false;
+		}
+	}
+
+	async function getDogs(ids: string[]) {
+		loadingDogs = true;
+		console.log(ids);
+		try {
+			const response = await Api.getDogs(ids);
+			if (response.ok) {
+				const body = await response.json();
+				console.log('BODY', body);
+				dogs = body;
+			}
+		} catch (err) {
+			console.error(err);
+		} finally {
+			loadingDogs = false;
+		}
+	}
+</script>
+
 <h1>Fetch a Pet!</h1>
+{#if loadingDogs || loadingSearch}
+	<h1>Loading</h1>
+{:else if dogs.length > 0}
+	<ul>
+		{#each dogs as dog}
+			<li>{dog.name}</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No dogs found.</p>
+{/if}
