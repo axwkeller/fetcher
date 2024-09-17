c9bea65bb0043ab5902b3033b93c8e5708ab18a2
Alex Keller <axwkeller@gmail.com>
2024-09-17 11:38:10 -0600

    Prevent hitting real API in specs

--- src/lib/utils/Api.ts
+++ src/lib/utils/Api.ts
@@ -1,10 +1,13 @@
-const BASE_URL = 'https://frontend-take-home-service.fetch.com';
 export interface LoginParams {
 	name: string;
 	email: string;
 }
 
 export default class Api {
+	private static get baseUrl(): string {
+		return process.env.NODE_ENV === 'test' ? '' : 'https://frontend-take-home-service.fetch.com';
+	}
+
 	static async login({ name, email }: LoginParams) {
 		return this.fetch('/auth/login', {
 			method: 'POST',
@@ -14,9 +17,9 @@ export default class Api {
 			})
 		});
 	}
-	private static async fetch(endpoint: string, options: RequestInit = {}) {
-		const url = `${BASE_URL}${endpoint}`;
 
+	private static async fetch(endpoint: string, options: RequestInit = {}) {
+		const url = `${this.baseUrl}${endpoint}`;
 		const headers: Record<string, string> = {
 			'Content-Type': 'application/json',
 			...(options.headers as Record<string, string>)
